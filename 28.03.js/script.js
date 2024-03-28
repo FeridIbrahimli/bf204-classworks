a636eb15ff08ebdb8f4ba4eafcd436cb0c713e44
FeridIbrahimli <ferid.ibrahimli3101@gmail.com>
2024-03-28 19:02:32 +0400

    master

---
+++ 28.03.js/script.js
@@ -0,0 +1,126 @@
+// TASK1
+// let a;
+// a = +window.prompt("yasi daxil edin");
+// if (a < 125) {
+//   if (a >= 65) {
+//     console.log("xeyrli olsun, qocalmisan");
+//   } else {
+//     console.log("cavansan hele");
+//   }
+// } else {
+//   console.log("olmusen");
+// }
+// TASK2
+// let a;
+//  a = +window.prompt("ededi daxil edin");
+//  if (a%2==0) {
+//     console.log("eded cutdur")
+
+//  }
+//  else{
+//     console.log("eded tekdir")
+//  }
+// TASK3
+// let month;
+// month = window.prompt("enter month");
+// switch (month) {
+//   case "dekabr":
+//   case "yanvar":
+//   case "fevral":
+//     console.log("winter");
+//     break;
+//   case "mart":
+//   case "aprel":
+//   case "may":
+//     console.log("spring");
+//     break;
+//   case "iyun":
+//   case "iyul":
+//   case "avqust":
+//     console.log("summer");
+//     break;
+//   case "sentyabr":
+//   case "oktyabr":
+//   case "noyabr":
+//     console.log("autumn");
+//     break;
+//   default:
+//     console.log("bele ay yoxdur");
+//     break;
+// }
+// TASK4
+// let a;
+// a = +window.prompt("ededi daxil edin");
+
+// if (a % 3 == 0 && a % 5 == 0) {
+//     console.log("3e ve 5e tam bolunur")
+    
+// }
+// else{
+//     console.log("bolunmur")
+// }
+// TASK5
+// let a;
+// a = +window.prompt("ededi daxil edin");
+// if (a % 2 == 0) {
+//     console.log("2e tam bolunur")
+    
+// }
+// else{
+//     console.log(a % 2)
+// }
+// TASK6
+// let a= +window.prompt("1ci ededi daxil edin");
+// let b= +window.prompt("2ci ededi daxil edin");
+// if (a*b>0) {
+//     console.log("musbetdir")
+    
+// }
+// else{
+//     console.log("menfidir")
+// }
+// TASK7
+// let a= +window.prompt("ededi daxil edin");
+// if (a>0) {
+//     if (a>0 && a<100) {
+//         console.log("eded 1-100 araligindedir")
+//     }
+    
+// }
+// else{
+//     console.log("bu araliqda deyil")
+// }
+// TASK8---------------------------------------------------------------------------
+// let a= +window.prompt("a ededi daxil edin");
+// let b= +window.prompt("b ededi daxil edin");
+// let c= +window.prompt("c ededi daxil edin");
+// if (a>b && a>c) {
+//     console.log("a en boyukdur")
+// }
+// else if(b>a && b>c){
+//     console.log("b en boyukdur")
+// }
+// else{
+//     console.log("c en boyukdur")
+// }
+// TASk10--------------------------------------------------------------------------
+// let bal;
+// bal= +window.prompt("bali daxil edin");
+// if (bal>90 && bal<=100) {
+//     console.log("A")
+// }
+// else if (bal>80 && bal<=90) {
+//     console.log("B")
+// }
+// else if (bal>70 && bal<=80) {
+//     console.log("C")
+// }
+// else if (bal>60 && bal<=70) {
+//     console.log("D")
+// }
+// else if (bal>50 && bal<=60) {
+//     console.log("E")
+// }
+// else{
+//     console.log("F")
+// }
