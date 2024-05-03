a8b48878a5e1ec3ece83eedcd42924664547d7cf
FeridIbrahimli <ferid.ibrahimli3101@gmail.com>
2024-05-03 19:35:24 +0400

    elebele

---
+++ 03.05-react-classroom/classroom-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Login from './companents/Login'
+
+function App() {
+
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default App
